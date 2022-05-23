e71bdb45a3b7cfdc27f62cd10faf0ab271f08181
Anna <annabutkova94@gmail.com>
2022-05-23 21:57:44 +0500

    fix: task 1 replace readFile with readStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,18 +2,9 @@ const fs = require('fs');
 const path = require('node:path');
 const absolutePath = path.join(__dirname, '/text.txt');
 
-/* fs.promises.readFile(absolutePath, 'utf8')
-  .then((data) => {
-    console.log(data);
-  }) 
-  .catch(console.error); */
-  
-async function read (file) {
-  try {
-    let a = await fs.promises.readFile(file, 'utf-8');
-    console.log(a);
-  } catch (error) {
-    console.error(error);
-  }
-}
-read(absolutePath);
+
+const stream = fs.createReadStream(absolutePath);
+
+stream.on('data', function (chunk) {
+  console.log(chunk.toString());
+});
